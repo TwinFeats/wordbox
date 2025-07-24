38de53e6397e4a2f8aa350f4af91c03f185a6cb3
Kent Smotherman <kent@twinfeats.com>
2025-07-24 17:49:14 -0500

    Add persistent game state

--- wordbox.js
+++ wordbox.js
@@ -1,119 +1,161 @@
-var minLen = 4;
-var word = "";
-var solution = {};
-var wordlist = [];
-var letters = "";
+var gameState = {
+	minLen: 4,
+	word: "",
+	solution: {},
+	wordlist: [],
+	letters: "",
+	orientations: [],
+	lastcube: null,
+	foundWords: [],
+	playing: false,
+	gameover: false,
+	seed: 0,
+	total: 0,
+	time: 300000,
+	options: {
+		size: 5,
+		random: true
+	},
+};
+
+var isMobile = navigator.maxTouchPoints > 1;
 var rnd = new Srand();
-var lastcube = null;
-var foundWords = [];
-var playing = false;
-var gameover = false;
-var seed = 0;
-var total = 0;
-var time = 300000;
-var lasttime = 0;
-var options = {
-	size: 5,
-	random: true
-}
 var goodPlay;
 var badPlay;
-var isMobile = navigator.maxTouchPoints > 1;
+var lasttime = 0;
+var timerCount = 0;
 
 function updateGame() {
-	var game = '/wordbox/?game=' + options.size + "" + seed;
-	history.replaceState(null, 'Wordbox Redux', game);
+	var game = '/wordbox/?game=' + gameState.options.size + "" + gameState.seed;
+	if (isMobile) {
+//		history.replaceState(null, 'Wordbox Redux', game);
+	}
 	navigator.clipboard.writeText("https://kidjuice.com"+game);
 }
 
-function init(gameseed) {
-	try {
-	if (isMobile) {
-		document.getElementById("pause").firstElementChild.innerHTML = "Tap to unpause";
-	} else {
-		document.getElementById("pause").firstElementChild.innerHTML = "Click to unpause";
-	}
-	goodPlay = document.getElementById("goodPlay");
-	badPlay = document.getElementById("badPlay");
-	lasttime = 0;
-	gameover = false;
-	word = "";
-	solution = {};
-	wordlist = [];
-	letters = "";
-	lastcube = null;
-	foundWords = [];
-	playing = false;
-	total = 0;
-	time = 300000;
+function restoreGame(savedGame) {
+	gameState = JSON.parse(savedGame);
+	gameState.playing = false;
+	pause();
+
+	initBoard();
+
+	//reinit appropriately here
 	document.getElementById("newgame").disabled = false;
 	document.querySelector("#timer").innerHTML = convertTime();
 	document.querySelector("#words").innerHTML = "";
 	var totalhtml = document.getElementById("total");
-	totalhtml.innerHTML = total;
+	totalhtml.innerHTML = gameState.total;
+
+	var cubeelements = document.querySelectorAll("#board > * > span");
+	for (var i = 0; i < gameState.letters.length; i++) {
+		var theletter = gameState.letters.charAt(i);
+		cubeelements[i].classList.add(theletter);
+		if (theletter == 'Q') theletter += "u"
+		cubeelements[i].innerHTML = theletter;
+		cubeelements[i].classList.add("orientation" + gameState.orientations[i]);
+	}
+	updateBoardSize(gameState.options.size);
+	for (var i=0; i<gameState.foundWords.length;i++) {
+		addWord(gameState.foundWords[i], scoreWord(gameState.foundWords[i]));
+	}
+}
+
+function init(gameseed) {
+	goodPlay = document.getElementById("goodPlay");
+	badPlay = document.getElementById("badPlay");
+	lasttime = 0;
+	var savedGame = localStorage.getItem("savedGame");
+	if (savedGame) {
+		restoreGame(savedGame);
+		return;
+	}
+	try {
+	gameState.gameover = false;
+	gameState.word = "";
+	gameState.solution = {};
+	gameState.wordlist = [];
+	gameState.letters = "";
+	gameState.lastcube = null;
+	gameState.foundWords = [];
+	gameState.playing = false;
+	gameState.total = 0;
+	gameState.time = 300000;
+	gameState.letters = "";
+	document.getElementById("newgame").disabled = false;
+	document.querySelector("#timer").innerHTML = convertTime();
+	document.querySelector("#words").innerHTML = "";
+	var totalhtml = document.getElementById("total");
+	totalhtml.innerHTML = gameState.total;
 	if (!gameseed) {
+		
 		var loc = window.location.search;
 		var idx = loc.lastIndexOf("game=");
 		if (idx >= 0) {
 			gameseed = parseInt(loc.substring(idx + 6));
-			options.size = parseInt(loc.substring(idx + 5, idx + 6));
+			gameState.options.size = parseInt(loc.substring(idx + 5, idx + 6));
 		} else {
 			updateBoardSize(5);
 		}
 	}
 
-	if (options.size == 6) {
-		time = 360000;
-		minLen = 5;
+	if (gameState.options.size == 6) {
+		gameState.time = 360000;
+		gameState.minLen = 5;
 		document.getElementById("board5").classList.remove("selected");
 		document.getElementById("board6").classList.add("selected");
 	} else {
-		minLen = 4;
+		gameState.minLen = 4;
 		document.getElementById("board5").classList.add("selected");
 		document.getElementById("board6").classList.remove("selected");
 	}
 	if (gameseed) {
-		seed = gameseed;
-		rnd = new Srand(seed);
+		gameState.seed = gameseed;
+		rnd = new Srand(gameState.seed);
 		updateGame();
-		var b = document.getElementById("board");
-		var html = "";
-		for (var i = 0; i < options.size * options.size; i++) {
-			html += '<div><span></span></div>';
-		}
-		b.innerHTML = html;
-		b.className = "";
-		if (options.size == 6) {
-			b.className = "board6";
-		}
-		var cubes = document.querySelectorAll("#board > *");
-		for (var i = 0; i < cubes.length; i++) {
-			var r = Math.floor(i / options.size);
-			var c = i % options.size;
-			var cube = cubes[i];
-			cube.dataset.row = r;
-			cube.dataset.col = c;
-		}
+		initBoard();
 		genPuzzle();
 		solve();
-		wordlist = Object.keys(solution);
-		wordlist.sort((w1, w2) => w2.length - w1.length);
-		document.getElementById("gamenumber").innerHTML = seed;
+		gameState.wordlist = Object.keys(gameState.solution);
+		gameState.wordlist.sort((w1, w2) => w2.length - w1.length);
+		document.getElementById("gamenumber").innerHTML = gameState.seed;
 		document.getElementById("pausebutton").innerHTML = "Pause";
 		pause();
 	}
 	} catch (e) {
+		console.log(e);
 			alert(e);
 		}
 }
 
+function initBoard() {
+	var b = document.getElementById("board");
+	var html = "";
+	for (var i = 0; i < gameState.options.size * gameState.options.size; i++) {
+		html += '<div><span></span></div>';
+	}
+	b.innerHTML = html;
+	b.className = "";
+	if (gameState.options.size == 6) {
+		b.className = "board6";
+	}
+	var cubes = document.querySelectorAll("#board > *");
+	for (var i = 0; i < cubes.length; i++) {
+		var r = Math.floor(i / gameState.options.size);
+		var c = i % gameState.options.size;
+		var cube = cubes[i];
+		cube.dataset.row = r;
+		cube.dataset.col = c;
+	}
+}
+
 function updateBoardStraight() {
-	options.random = !options.random;
-	document.getElementById("straight").innerHTML = options.random ? "Straight" : "Random";
+	gameState.options.random = !gameState.options.random;
+	document.getElementById("straight").innerHTML = gameState.options.random ? "Straight" : "Random";
 	var cubeelements = document.querySelectorAll("#board > * > span");
 	var i = 0;
 	for (var i = 0; i < cubeelements.length; i++) {
-		if (options.random) {
+		if (gameState.options.random) {
 			cubeelements[i].classList.remove("orientation0");
 		} else {
 			cubeelements[i].classList.add("orientation0");
@@ -128,8 +170,8 @@ function hide(event) {
 }
 
 function updateBoardSize(size) {
-	options.size = size;
-	if (options.size == 6) {
+	gameState.options.size = size;
+	if (gameState.options.size == 6) {
 		document.getElementById("board5").classList.remove("selected");
 		document.getElementById("board6").classList.add("selected");
 	} else {
@@ -146,16 +188,17 @@ function unpause(event) {
 		goodPlay.pause();
 		var pause = document.querySelector("#pause");
 		pause.classList.add("hidden");
-		playing = true;
+		gameState.playing = true;
 		lasttime = new Date().getTime();
+		timerCount = 0;
 		startTimer();
 	// }
 	// event.preventDefault();
 }
 
 function pause() {
-	if (!gameover) {
-		playing = false;
+	if (!gameState.gameover) {
+		gameState.playing = false;
 		var pause = document.querySelector("#pause");
 		pause.classList.remove("hidden");
 	} else {
@@ -165,7 +208,7 @@ function pause() {
 
 function doNewGame() {
 	closeConfirm();
-	playing = false;
+	gameState.playing = false;
 	newgame();
 }
 
@@ -174,28 +217,29 @@ function closeConfirm() {
 }
 
 function newgame() {
-	if (playing) {
-		pause();
+	if (gameState.playing) {
+		gameState.playing = false;
 		document.getElementById('confirm').classList.add('show');
 		return;
 	}
-	gameover = false;
-	playing = false;
+	localStorage.clear();
+	gameState.gameover = false;
+	gameState.playing = false;
 	pause();
-	rnd = new Srand();
-	seed = Math.floor(rnd.inRange(0, 1000000000));
-	init(seed);
+	gameState.rnd = new Srand();
+	gameState.seed = Math.floor(rnd.inRange(0, 1000000000));
+	init(gameState.seed);
 }
 
 function startdrag(event) {
 	event.preventDefault();
 	event.target.releasePointerCapture(event.pointerId);
-	if (!playing) return;
+	if (!gameState.playing) return;
 	var cube = findCube(event.pageX, event.pageY);
 	if (cube && !cube.classList.contains("hilite")) {
-		lastcube = cube;
+		gameState.lastcube = cube;
 		cube.classList.add("hilite");
-		word = cube.children[0].textContent.toUpperCase();
+		gameState.word = cube.children[0].textContent.toUpperCase();
 		updateWord();
 	}
 }
@@ -203,61 +247,67 @@ function startdrag(event) {
 function dragging(event) {
 	event.preventDefault();
 	event.target.releasePointerCapture(event.pointerId);
-	if (!playing) return;
-	if (!lastcube) return;
+	if (!gameState.playing) return;
+	if (!gameState.lastcube) return;
 	var cube = findCube(event.pageX, event.pageY);
 	if (!cube) return;
-	if (cube.dataset.row == lastcube.dataset.row && cube.dataset.col == lastcube.dataset.col) return;
+	if (cube.dataset.row == gameState.lastcube.dataset.row && cube.dataset.col == gameState.lastcube.dataset.col) return;
 	if (isAdjacent(cube) && !cube.classList.contains("hilite")) {
 		cube.classList.add("hilite");
-		word += cube.children[0].textContent.toUpperCase();
+		gameState.word += cube.children[0].textContent.toUpperCase();
 		updateWord();
-		lastcube = cube;
+		gameState.lastcube = cube;
 	}
 }
 
 function isAdjacent(cube) {
-	return (Math.abs(parseInt(lastcube.dataset.row) - parseInt(cube.dataset.row)) <= 1 && Math.abs(parseInt(lastcube.dataset.col) - parseInt(cube.dataset.col)) <= 1);
+	return (Math.abs(parseInt(gameState.lastcube.dataset.row) - parseInt(cube.dataset.row)) <= 1 && Math.abs(parseInt(gameState.lastcube.dataset.col) - parseInt(cube.dataset.col)) <= 1);
+}
+
+function addWord(word, score) {
+	var wordshtml = document.getElementById("words");
+	wordshtml.innerHTML = wordshtml.innerHTML + "<div><span>" + word + "</span><span>" + score + "</span></div>";
+
 }
 
 function stopdrag(event) {
 	event.preventDefault();
-	if (!playing) return;
+	if (!gameState.playing) return;
 	var cubes = document.querySelectorAll(".hilite");
 	for (var i = 0; i < cubes.length; i++) {
 		cubes[i].classList.remove('hilite');
 	}
-	if (word.length >= minLen) {
-		if (!foundWords.includes(word)) {
-			if (words.includes(word.toLowerCase())) {
+	if (gameState.word.length >= gameState.minLen) {
+		if (!gameState.foundWords.includes(gameState.word)) {
+			if (words.includes(gameState.word.toLowerCase())) {
 				playGood();
 				blink(4, "goodword");
-				foundWords.push(word);
-				var score = scoreWord();
-				total += score;
-				var wordshtml = document.getElementById("words");
-				wordshtml.innerHTML = wordshtml.innerHTML + "<div><span>" + word + "</span><span>" + score + "</span></div>";
+				gameState.foundWords.push(gameState.word);
+				var score = scoreWord(gameState.word);
+				gameState.total += score;
+				addWord(gameState.word, score);
 				var totalhtml = document.getElementById("total");
-				totalhtml.innerHTML = total;
+				totalhtml.innerHTML = gameState.total;
+				localStorage.setItem("savedGame", JSON.stringify(gameState));
 			} else {
 				playBad();
 				blink(4, "badword");
 				var w = document.querySelector("#word");
-				w.innerHTML = word + " 0";
+				w.innerHTML = gameState.word + " 0";
 			}
 		} else {
 			playBadDup();
 			blink(4, "dupword");
 			var w = document.querySelector("#word");
-			w.innerHTML = word + " dup";
+			w.innerHTML = gameState.word + " dup";
 		}
 	} else {
 		playBad();
 		blink(4, "badword");
 		var w = document.querySelector("#word");
-		w.innerHTML = word + " 0";
+		w.innerHTML = gameState.word + " 0";
 	}
-	lastcube = null;
+	gameState.lastcube = null;
 }
 
 function blink(count, cssClass) {
@@ -287,9 +337,9 @@ function canceldrag(event) {
 	for (var i = 0; i < cubes.length; i++) {
 		cubes[i].classList.remove('hilite');
 	}
-	word = "";
+	gameState.word = "";
 	updateWord();
-	lastcube = null;
+	gameState.lastcube = null;
 }
 
 function findCube(x, y) {
@@ -307,7 +357,7 @@ function findCube(x, y) {
 	return null;
 }
 
-function scoreWord() {
+function scoreWord(word) {
 	var score = Math.pow(2, word.length - 4);
 	var w = document.querySelector("#word");
 	w.innerHTML = word + " " + score;
@@ -316,7 +366,7 @@ function scoreWord() {
 
 function updateWord() {
 	var w = document.querySelector("#word");
-	w.innerHTML = word;
+	w.innerHTML = gameState.word;
 }
 
 function genPuzzle() {
@@ -324,12 +374,11 @@ function genPuzzle() {
 	var cubes6 = ["CEIPST", "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM", "AEEGMU", "AEGMNN", "AFIRSY", "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM", "AEEGMU", "AEGMNN", "AFIRSY", "BJKQXZ", "CCENST", "DDHNOT", "CEIILT", "CEILPT", "CEIPST", "DHHLOR", "DHLNOR", "EIIITT", "EMOTTT", "ENSSSU", "FIPRSY", "GORRVW", "IPRRRY", "NOOTUW", "OOOTTU", "DHLNOR", "OOOTTU", "DHLNOR"];
 	var cubes = null;
 
-	if (options.size == 6) {
+	if (gameState.options.size == 6) {
 		cubes = cubes6;
 	} else {
 		cubes = cubes5;
 	}
-	letters = "";
 	var cubeelements = document.querySelectorAll("#board > * > span");
 	var i = 0;
 	while (cubes.length > 0) {
@@ -340,18 +389,20 @@ function genPuzzle() {
 		cubes.splice(cube, 1);
 		var letter = Math.floor(rnd.inRange(0, cletters.length));
 		var theletter = cletters.charAt(letter);
-		letters += theletter;
+		gameState.letters += theletter;
 		cubeelements[i].classList.add(theletter);
 		if (theletter == 'Q') theletter += "u"
 		cubeelements[i].innerHTML = theletter;
-		cubeelements[i].classList.add("orientation" + Math.floor(rnd.inRange(0, 4)));
+		var o = Math.floor(rnd.inRange(0, 4));
+		gameState.orientations.push(o);
+		cubeelements[i].classList.add("orientation" + o);
 		i++;
 	}
 }
 
 function solve() {
-	solution = {};
-	var n = Math.round(Math.sqrt(letters.length));
+	gameState.solution = {};
+	var n = Math.round(Math.sqrt(gameState.letters.length));
 	var cubes = new Array(n);
 	for (var i = 0; i < n; i++) {
 		cubes[i] = new Array(n);
@@ -362,7 +413,7 @@ function solve() {
 	var cubelist = [];
 	for (var r = 0; r < n; r++) {
 		for (var c = 0; c < n; c++) {
-			cubes[r][c] = new Cube(letters.charAt(i++), r, c);
+			cubes[r][c] = new Cube(gameState.letters.charAt(i++), r, c);
 			var ch = cubes[r][c].letter;
 			cubelist = map[ch];
 			if (!cubelist) {
@@ -375,7 +426,7 @@ function solve() {
 	ch = null;
 	for (var n = 0; n < words.length; n++) {
 		var w = words[n].toUpperCase();
-		if (w.length >= minLen && !solution[w]) {
+		if (w.length >= gameState.minLen && !gameState.solution[w]) {
 			var cr = w.charAt(0);
 			if (ch == null || ch != cr) {
 				ch = cr;
@@ -401,7 +452,7 @@ function processCube(cubes, r, c, word, idx) {
 		cubes[r][c].tried = true;
 		idx++;
 		if (idx == word.length) {
-			solution[word] = word;
+			gameState.solution[word] = word;
 			return true;
 		}
 		if (letter == 'Q') {
@@ -409,7 +460,7 @@ function processCube(cubes, r, c, word, idx) {
 			if (letter == 'U') {
 				idx++;
 				if (idx == word.length) {
-					solution[word] = word;
+					gameState.solution[word] = word;
 					return true;
 				}
 			}
@@ -438,17 +489,17 @@ function processCube(cubes, r, c, word, idx) {
 }
 
 function gameOver() {
-	playing = false;
-	gameover = true;
+	gameState.playing = false;
+	gameState.gameover = true;
 	var list = document.getElementById("endgame");
 	var text = "";
-	for (i = 0; i < wordlist.length; i++) {
-		if (foundWords.includes(wordlist[i])) {
+	for (i = 0; i < gameState.wordlist.length; i++) {
+		if (gameState.foundWords.includes(gameState.wordlist[i])) {
 			text += "<div class='found'>";
 		} else {
 			text += "<div>";
 		}
-		text += wordlist[i] + " " + Math.pow(2, wordlist[i].length - 4) + "</div>";
+		text += gameState.wordlist[i] + " " + Math.pow(2, gameState.wordlist[i].length - 4) + "</div>";
 	}
 	list.innerHTML = text;
 	document.getElementById("endgame").classList.add("visible");
@@ -456,12 +507,16 @@ function gameOver() {
 }
 
 function startTimer() {
-	if (!playing) return;
+	if (!gameState.playing) return;
 	var now = new Date().getTime();
 	var diff = now - lasttime;
 	lasttime = now;
-	time -= diff;
-	if (time < 0) {
+	gameState.time -= diff;
+	if (++timerCount >= 10) {
+		localStorage.setItem("savedGame", JSON.stringify(gameState));
+		timerCount = 0;
+	}
+	if (gameState.time < 0) {
 		gameOver();
 		return;
 	}
@@ -471,7 +526,7 @@ function startTimer() {
 }
 
 function convertTime() {
-	var secs = Math.floor(time / 1000);
+	var secs = Math.floor(gameState.time / 1000);
 	var m = "" + Math.floor(secs / 60);
 	var s = secs % 60;
 	if (s < 10) {
